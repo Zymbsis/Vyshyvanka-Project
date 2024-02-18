04654278e9bb2a24e02cf6c9d9d6145b3f72f744
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2024-02-18 22:09:59 +0100

    'update'

--- src/partials/js/our-collection.js
+++ src/partials/js/our-collection.js
@@ -26,7 +26,7 @@ function createLightboxInstance(e) {
     <button class="lightbox-close-button" type="button"><svg width="26" height="26" class="lightbox-close-icon"><use href="./img/icons.svg#menu-close">
     </use><svg></button>
     <div class="lightbox-img-wrapper">
-
+    <picture class="lightbox-img" ${e.target.parentNode.innerHTML}</picture>
     <ul class="lightbox-size-list">
     ${markupLightboxSizeChart([
       'XXS',
@@ -78,6 +78,3 @@ mainContainer.addEventListener('click', e => {
   createLightboxInstance(e).show();
   console.log(document.querySelector('.lightbox-size-list'));
 });
-
-// <picture class="lightbox-img" ${e.target.parentNode.innerHTML}
-// </picture></picture>
